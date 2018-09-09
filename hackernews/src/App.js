e9507c4295ee78c9349a0a29985fbf03316bfc34
Nicholas Rampton <nick.rampton@gmail.com>
2018-09-08 20:32:39 -0400

    updating dependencies

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -28,61 +28,85 @@ const list = [
   },
 ];
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
 class App extends Component {
-
   constructor(props) {
     super(props);
+
     this.state = {
       list: list,
-      searchTerm: '',
-    };
-    this.onDismiss = this.onDismiss.bind(this);
-    this.onSearchChange = this.onSearchChange.bind(this);
-  }
-
-  onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectId !== id);
-    this.setState({ list: updatedList, })
-  }
-
-  onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value });
+    }
   }
 
   render() {
     return (
-      <div className='App'>
-        <form>
-          <input 
-            type="text"
-            onChange={this.onSearchChange}
-          />
-        </form>
-        <h1>Books!</h1>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map((item) => {
-          return (
-            <div key={item.objectId}>
-              <p><strong>Title:</strong><i> {item.title}</i></p>
-              <p>Author: {item.author}</p>
-              <p>Number of Pages: {item.pages}</p>
-              <p>Year of Publication: {item.pubYear}</p>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectId)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          );
-        })}
+      <div className="App">
+        {this.state.list.map(item =>
+          <div key={item.objectId}>
+            <span><i> {item.title}</i></span>
+            <span> {item.author}</span>
+            <span> {item.pages}pp.</span>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+// const isSearched = searchTerm => item =>
+//   item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       list: list,
+//       searchTerm: '',
+//     };
+//     this.onDismiss = this.onDismiss.bind(this);
+//     this.onSearchChange = this.onSearchChange.bind(this);
+//   }
+
+//   onDismiss(id) {
+//     const updatedList = this.state.list.filter(item => item.objectId !== id);
+//     this.setState({ list: updatedList, })
+//   }
+
+//   onSearchChange(event) {
+//     this.setState({ searchTerm: event.target.value });
+//   }
+
+//   render() {
+//     return (
+//       <div className='App'>
+//         <form>
+//           <input 
+//             type="text"
+//             onChange={this.onSearchChange}
+//           />
+//         </form>
+//         <h1>Books!</h1>
+//         {this.state.list.filter(isSearched(this.state.searchTerm)).map((item) => {
+//           return (
+//             <div key={item.objectId}>
+//               <p><strong>Title:</strong><i> {item.title}</i></p>
+//               <p>Author: {item.author}</p>
+//               <p>Number of Pages: {item.pages}</p>
+//               <p>Year of Publication: {item.pubYear}</p>
+//               <span>
+//                 <button
+//                   onClick={() => this.onDismiss(item.objectId)}
+//                   type="button"
+//                 >
+//                   Dismiss
+//                 </button>
+//               </span>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
